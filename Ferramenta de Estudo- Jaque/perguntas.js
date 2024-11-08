40f31d21937c944e040a94bc480092c37338155c
Jair3C <00001113048426sp@al.educacao.sp.gov.br>
2024-11-08 10:37:44 -0300

    Add files via upload

---
+++ Ferramenta de Estudo- Jaque/perguntas.js
@@ -0,0 +1,24 @@
+
+criaCartao(
+    'Programação',
+    'O que é JAVA Script?',
+    'O JAVA Script é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da Espanha ?',
+    'A capital da Espanha é Madri'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma linha de código?',
+    'Uma linha de código é um comando dado a programação.'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz Porta em Inglês?',
+    'Porta em ingles é Door'
+)
